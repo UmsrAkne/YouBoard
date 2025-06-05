cbe00f1d88bdd8b8fe7d688709712df44dd55f03
UmsrAkne <sibata486@gmail.com>
2025-06-05 20:49:12 +0900

    feat: add and apply ItemTemplate for issue list display
    - Defined ItemTemplate for IssueWrapper in IssueListView
    - Styled list items with dark background and white text

--- YouBoard/Models/IssueWrapper.cs
+++ YouBoard/Models/IssueWrapper.cs
@@ -6,9 +6,15 @@ namespace YouBoard.Models
     {
         private string id = string.Empty;
         private string title;
+        private string status;
+        private bool isComplete;
 
         public string Id { get => id; set => SetProperty(ref id, value); }
 
         public string Title { get => title; set => SetProperty(ref title, value); }
+
+        public string Status { get => status; set => SetProperty(ref status, value); }
+
+        public bool IsComplete { get => isComplete; set => SetProperty(ref isComplete, value); }
     }
 }
