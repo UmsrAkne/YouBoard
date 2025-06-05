3f9dafefa9a0a5583265aeb6cc52150ede773eb3
UmsrAkne <sibata486@gmail.com>
2025-06-05 18:55:03 +0900

    feat: raise ItemChosen event when a project is explicitly selected
    - Added ItemChosen event to ITabViewModel and its implementations
    - Introduced ProjectChosenCommand in ProjectListViewModel
      to notify explicit user action (e.g. double-click) on a project
    - Updated ProjectListView.xaml to bind double-click behavior to the command

--- YouBoard/ViewModels/IssueListViewModel.cs
+++ YouBoard/ViewModels/IssueListViewModel.cs
@@ -1,3 +1,4 @@
+using System;
 using Prism.Mvvm;
 
 namespace YouBoard.ViewModels
@@ -5,8 +6,12 @@ namespace YouBoard.ViewModels
     // ReSharper disable once ClassNeverInstantiated.Global
     public class IssueListViewModel : BindableBase, ITabViewModel
     {
+        public event EventHandler ItemChosen;
+
         public string TabType { get; set; } = "IssueList";
 
         public string Header { get; set; }
+
+        public object SelectedItem { get; set; }
     }
 }
