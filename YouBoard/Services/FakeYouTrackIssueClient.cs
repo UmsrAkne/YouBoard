cbb59058703f00870791492a4bd91bd0beac714a
UmsrAkne <sibata486@gmail.com>
2025-06-06 17:15:33 +0900

    feat: extended issue retrieval logic to allow skipping an arbitrary number of recent issues within a project.
    feat: 課題の取得処理を拡張。プロジェクト内の最新課題から任意件数をスキップして取得可能にした。

--- YouBoard/Services/FakeYouTrackIssueClient.cs
+++ YouBoard/Services/FakeYouTrackIssueClient.cs
@@ -6,7 +6,7 @@ namespace YouBoard.Services
 {
     public class FakeYouTrackIssueClient : IYouTrackIssueClient
     {
-        public Task<List<IssueWrapper>> GetIssuesByProjectAsync(string projectShortName, int count = 0)
+        public Task<List<IssueWrapper>> GetIssuesByProjectAsync(string projectShortName, int count = 0, int skip = 0)
         {
             var dummy = new List<IssueWrapper>
             {
--- YouBoard/Services/IYouTrackIssueClient.cs
+++ YouBoard/Services/IYouTrackIssueClient.cs
@@ -6,6 +6,6 @@ namespace YouBoard.Services
 {
     public interface IYouTrackIssueClient
     {
-        Task<List<IssueWrapper>> GetIssuesByProjectAsync(string projectShortName, int count = 0);
+        Task<List<IssueWrapper>> GetIssuesByProjectAsync(string projectShortName, int count = 0, int skip = 0);
     }
 }
