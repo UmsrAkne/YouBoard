31e34e8985630f109aeae74db5e0dcd2ae064414
UmsrAkne <sibata486@gmail.com>
2025-06-28 21:31:27 +0900

    feat: display comment timestamp in list

--- YouBoard/Models/IssueCommentWrapper.cs
+++ YouBoard/Models/IssueCommentWrapper.cs
@@ -1,13 +1,17 @@
+using System;
+
 namespace YouBoard.Models
 {
     public class IssueCommentWrapper
     {
         public string Id { get; set; }
-        
+
         public string Text { get; set; }
 
         public string AuthorName { get; set; }
 
         public long Created { get; set; }
+
+        public DateTime CreatedAt => DateTimeOffset.FromUnixTimeMilliseconds(Created).LocalDateTime;
     }
 }
