bc79275b9e73721528e94bb048c3ce5c6ef577f8
UmsrAkne <sibata486@gmail.com>
2025-07-03 16:40:32 +0900

    chore: remove unnecessary field

--- YouBoard/ViewModels/IssueListViewModel.cs
+++ YouBoard/ViewModels/IssueListViewModel.cs
@@ -13,7 +13,6 @@ namespace YouBoard.ViewModels
     public class IssueListViewModel : BindableBase, ITabViewModel
     {
         private readonly IYouTrackIssueClient client;
-        private readonly string projectName = string.Empty;
         private readonly string projectShortName = string.Empty;
         private IssueWrapper pendingIssue = new ();
 
@@ -23,7 +22,7 @@ namespace YouBoard.ViewModels
 
         public IssueListViewModel(IYouTrackIssueClient client, ProjectWrapper project)
         {
-            projectName = project.Name;
+            var projectName = project.Name;
             projectShortName = project.ShortName;
             this.client = client;
 
