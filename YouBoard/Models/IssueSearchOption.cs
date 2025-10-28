b88c83b5caefa72b7852106affbb27d818ffa8c6
UmsrAkne <sibata486@gmail.com>
2025-10-28 18:15:20 +0900

    feat: extend IssueSearchOption with additional filters and pagination (non integrated)
    - Added `IsAscending`, `Limit`, `Offset` for pagination and sorting direction.
    - Introduced `SearchPattern` and `ProjectShortName` to support more refined search capabilities.

--- YouBoard/Models/IssueSearchOption.cs
+++ YouBoard/Models/IssueSearchOption.cs
@@ -7,6 +7,11 @@ namespace YouBoard.Models
         private bool isSortByCreatedDate;
         private bool isSortByTitle;
         private bool isSortByType;
+        private bool isAscending;
+        private int limit;
+        private int offset;
+        private string searchPattern = string.Empty;
+        private string projectShortName = string.Empty;
 
         public bool IsSortByCreatedDate
         {
@@ -17,5 +22,19 @@ namespace YouBoard.Models
         public bool IsSortByTitle { get => isSortByTitle; set => SetProperty(ref isSortByTitle, value); }
 
         public bool IsSortByType { get => isSortByType; set => SetProperty(ref isSortByType, value); }
+
+        public bool IsAscending { get => isAscending; set => SetProperty(ref isAscending, value); }
+
+        public int Limit { get => limit; set => SetProperty(ref limit, value); }
+
+        public int Offset { get => offset; set => SetProperty(ref offset, value); }
+
+        public string SearchPattern { get => searchPattern; set => SetProperty(ref searchPattern, value); }
+
+        public string ProjectShortName
+        {
+            get => projectShortName;
+            set => SetProperty(ref projectShortName, value);
+        }
     }
 }
