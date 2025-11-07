d758453af913b42aac929f69e08f5731a00c333d
UmsrAkne <sibata486@gmail.com>
2025-11-07 20:27:15 +0900

    fix: prevent inner ListBox from blocking outer scroll
     - Added PreviewMouseWheel handler to inner ListBox
     - Re-routed scroll events upward to parent elements using VisualTreeHelper.GetParent()
     - Removed fragile Parent chain logic
     - Ensured smooth and consistent scrolling through nested ListBoxes

--- YouBoard/Views/IssueListView.xaml.cs
+++ YouBoard/Views/IssueListView.xaml.cs
@@ -1,3 +1,9 @@
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
 namespace YouBoard.Views
 {
     public partial class IssueListView
@@ -5,6 +11,34 @@ namespace YouBoard.Views
         public IssueListView()
         {
             InitializeComponent();
+        }
+
+        private void InnerListBox_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            if (!e.Handled)
+            {
+                e.Handled = true;
+
+                var eventArg = new MouseWheelEventArgs(e.MouseDevice, e.Timestamp, e.Delta)
+                {
+                    RoutedEvent = MouseWheelEvent,
+                    Source = sender,
+                };
+
+                var current = sender as DependencyObject;
+
+                while (current != null)
+                {
+                    current = VisualTreeHelper.GetParent(current);
+                    if (current is not UIElement element)
+                    {
+                        continue;
+                    }
+
+                    element.RaiseEvent(eventArg);
+                    break;
+                }
+            }
         }
     }
 }
